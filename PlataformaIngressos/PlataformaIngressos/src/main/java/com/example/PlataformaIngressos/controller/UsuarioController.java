e1be62b0009da71de00079844ca16f647063dcc9
Wolgrand <wolgrand.tecdev@gmail.com>
2025-02-11 01:18:10 -0300

    update

---
+++ PlataformaIngressos/PlataformaIngressos/src/main/java/com/example/PlataformaIngressos/controller/UsuarioController.java
@@ -0,0 +1,4 @@
+package com.example.PlataformaIngressos.controller;
+
+public class UsuarioController {
+}
--- PlataformaIngressos/PlataformaIngressos/src/main/java/com/example/PlataformaIngressos/repository/EventoRepository.java
+++ PlataformaIngressos/PlataformaIngressos/src/main/java/com/example/PlataformaIngressos/repository/EventoRepository.java
@@ -2,9 +2,11 @@ package com.example.PlataformaIngressos.repository;
 
 import com.example.PlataformaIngressos.model.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EventoRepository extends JpaRepository<Evento,Long> {
 
     Optional<Evento> findByNome(String nome);
